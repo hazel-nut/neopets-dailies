9c3fd2d96c06f502d3508762cbb45de116f04a27
Hazel Virdó <github@virdo.name>
2015-01-03 22:40:38 -0500

    version

--- dailies.js
+++ dailies.js
@@ -7,7 +7,7 @@
 // @match        http://*.neopets.com/*
 // @match        http://neopets.com/*
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js
-// @version      2.0.12
+// @version      2.1.0
 // ==/UserScript==
 
 var bootScript = function(){
